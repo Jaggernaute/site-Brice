cd641330377cd74071788c352981b44f7a56814f
lucas <lucas.videcoq1@gmail.com>
2021-06-03 16:28:15 +0200

    done : - head ( a lot of tag) - test PHP tag

---
+++ README.md
@@ -0,0 +1,41 @@
+# Site USN76
+
+![logo](./images/logo-USN76.png)
+
+Need info ? Check the [Docs](bouillancecube-docs)
+| or [Create an issue](https://github.com/Jaggernaute/bouillancube/issues/new)
+
+<!-- tes badges ici -->
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/20a0176ef3714d799dd8e6189d95fc6e)](https://www.codacy.com/gh/Jaggernaute/bouillancube/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Jaggernaute/bouillancube&amp;utm_campaign=Badge_Grade)
+---
+
+## Table of Contents
+- [Getting Started](#getting-started)
+    - [Basic Usage](#basic-usage)
+      -[Features at a glance](#features-at-a-glance)
+    - [Road Map](#road-map)
+    - [License](#license)
+
+## Getting Started
+<!-- la ici tu me fais un beau petit texte genre sur les enjeux de ton plugin, pourquoi tu as fait ce plugin et tout le tralala -->
+.
+
+### Basic Usage
+<!-- ici tu me met comment on utilise ton truc -->
+\`\`\`
+
+\`\`\`
+
+## Features At A Glance
+<!--ici tu mets des petites images avec une description de ce que ton plugins branle dans la vie-->
+
+.
+
+### Road map
+If you want to see a new feature feel free to [create a new Issue](https://github.com/Jaggernaute/bouillancube/issues/new). Here are some features which are either under way or planned:
+
+- [x] detect millestone
+- [ ] give a reward when a millestone si completed
+
+### License
+Bouillancube is made available under the [MIT license]()
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="language" content="French">
+    <meta name="author" content="Jaggernaute">
+    <meta name="description"
+          content="L'USN76 est un club de foot basé sur la communauté de commune des villes soeurs région normandie, avec des équipes allant de la U7 au senior en passant par l'équipe feminine.">
+    <meta name="keywords" content="Foot, foot normandie, foot villes soeurs, foot district de normandie, USN76">
+
+    <!-- index in search engine -->
+    <meta name="robots" content="index,follow">
+    <!-- google specific -->
+    <meta name="googlebot" content="index,follow">
+    <meta name="google" content="nositelinkssearchbox">
+    <meta name="google" content="notranslate">
+
+    <!-- Facebook Open Graph markup -->
+    <meta property="og:locale" content="fr_FR">
+    <meta property="og:url" content=""> <!--WIP add URl -->
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="USN76 équipe de foot sur les villes soeurs">
+    <meta name="og:email" content=""/> <!--WIP add email address -->
+    <meta property="og:description"
+          content="L'USN76 est un club de foot basé sur la communauté de commune des villes soeurs région normandie, avec des équipes allant de la U7 au senior en passant par l'équipe feminine.">
+    <meta property="og:image" content=""> <!--WIP add image -->
+
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>USN 76</title>
+</head>
+<body>
+<?= "PHP works correctly YAY" ?>
+</body>
+</html>
